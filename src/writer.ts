e43aa65b8f99c0c59683aafbfc1562a2a9b06a2c
Pedro Dal Bello Carranza <pedro.d@sidi.org.br>
2023-04-28 16:39:39 -0300

    Minor fixes for better rewrited file visualization

--- src/writer.ts
+++ src/writer.ts
@@ -2,28 +2,33 @@ import * as fs from 'fs';
 
 
 export function writeBlocks(fileDescriptor: any, filePos: number, resources: any) {
-    resources.forEach((resource: any) => {
-      filePos=writeLines(fileDescriptor, filePos, resource.line, resource.tab);
-  
-      if(resource.lineBlock !== undefined){
-        filePos= writeBlocks(fileDescriptor, filePos, resource.lineBlock);
-      }
-    });
-    return filePos;
+  for (let i = 0; i < resources.length; i++) {
+    filePos = writeLines(fileDescriptor, filePos, resources[i].line, resources[i].tab);
+
+    if (resources[i].lineBlock) {
+      filePos = writeBlocks(fileDescriptor, filePos, resources[i].lineBlock);
+    }
+
+    if ((resources[i].mainType && resources[i].mainType !== 'attribution') // Current line is not attribution
+      || (resources[i + 1] && resources[i + 1].mainType && resources[i + 1].mainType !== 'attribution')) { // Current line is attribution but next is not
+      filePos += fs.writeSync(fileDescriptor, '\n', filePos, 'utf8'); // Add an extra blank line for better visualization
+    }
+  }
+  return filePos;
 }
 
-export function writeLines(fileDescriptor: any, filePos: number, line: any, tabNum: number){
-    let tabs = '';
+export function writeLines(fileDescriptor: any, filePos: number, line: any, tabNum: number) {
+  let tabs = '';
 
-    for (let i = 0; i < tabNum; i++) {
-      tabs+='\t';
-    }
-    filePos+=fs.writeSync(fileDescriptor, tabs, filePos,'utf8');
+  for (let i = 0; i < tabNum; i++) {
+    tabs += '\t';
+  }
+  filePos += fs.writeSync(fileDescriptor, tabs, filePos, 'utf8');
 
-    line.forEach((word: any) => {
-      filePos+=fs.writeSync(fileDescriptor, word.separator+word.value, filePos,'utf8');
-    });
-    filePos+=fs.writeSync(fileDescriptor, '\n', filePos, 'utf8');
-    
-    return filePos;
+  line.forEach((word: any) => {
+    filePos += fs.writeSync(fileDescriptor, word.separator + word.value, filePos, 'utf8');
+  });
+  filePos += fs.writeSync(fileDescriptor, '\n', filePos, 'utf8');
+
+  return filePos;
 }
