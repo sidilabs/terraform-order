40a62b14a149ce6274ddd8f5201992214638d08a
Rodolfo Dalla Costa <r.costa@sidi.org.br>
2023-07-13 16:19:43 -0300

    Implement sort methods for variables

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,8 @@ import { setStorageData } from './reservedKeywords';
 
 import { readArrayLines } from './utils';
 import { writeBlocks } from './writer';
+import { types } from 'util';
+import { stringify } from 'querystring';
 
 
 export function activate(context: vscode.ExtensionContext) {
@@ -42,14 +44,81 @@ export function activate(context: vscode.ExtensionContext) {
 
     const fileStrArr = fileStr.split('\n').filter((line) => line.trim());
     let arrResult = readArrayLines(fileStrArr);
+    let variableList: Array<object> = [];
+    let resourceList: Array<object> = [];
+    let datasourceList: Array<object> = [];
+    let typesMap = new Map<string, number[]>();
     for (let i = 0; i < arrResult.length; i++) {
       if (reservedMainKeywords.includes(arrResult[i].line[0].value)) {
         arrResult[i].mainType = arrResult[i].line[0].value;
       } else {
         arrResult[i].mainType = "attribution";
       }
+
+      let currValue: Array<any> = []
+
+      if(typesMap.has(arrResult[i].mainType)) {
+        currValue.push(typesMap.get(arrResult[i].mainType));
+      }
+
+      currValue.push(i);
+      typesMap.set(arrResult[i].mainType, currValue);
+
     }
     console.log(arrResult);
+
+    type orderingArray = {
+      element: any,
+      position: number
+    }
+
+    let dataList: any[] = []
+    let varList: any[] = []
+    let resList: any[] = []
+    
+    if(typesMap.has("data")) {
+      for (let i = 0; i < typesMap.get("data")!.length; i++) {
+        const item: orderingArray = {
+          element: arrResult[i],
+          position: i
+        }
+        dataList.push(item);
+      }
+
+      dataList.sort((d1, d2) => {
+        return d1.element.line[2].toLowerCase().localeCompare(d2.element.line[2].toLowerCase());
+      });
+    }
+
+    if(typesMap.has("variable")) {
+      for (let i = 0; i < typesMap.get("variable")!.length; i++) {
+        const item: orderingArray = {
+          element: arrResult[i],
+          position: i
+        }
+        varList.push(item);
+      }
+
+      varList.sort((d1, d2) => {
+        return d1.element.line[1].toLowerCase().localeCompare(d2.element.line[1].toLowerCase());
+      });
+    }   
+
+    if(typesMap.has("resource")) {
+      for (let i = 0; i < typesMap.get("resource")!.length; i++) {
+        const item: orderingArray = {
+          element: arrResult[i],
+          position: i
+        }
+        resList.push(item);
+      }
+
+      resList.sort((d1, d2) => {
+        return d1.element.line[2].toLowerCase().localeCompare(d2.element.line[2].toLowerCase());
+      });
+    }
+
+    
 
     arrResult.forEach((element) => {
       vscode.window.showInformationMessage('found elements: ' + element.line.join(' '));
