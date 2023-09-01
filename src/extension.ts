c016e7bbb10909d0e3ebcea750ccd0219e297ee8
Rodolfo Dalla Costa <r.costa@samsung.com>
2023-09-01 13:57:11 -0300

    Add original sample file to ensure there will awlays a copy of original

--- src/extension.ts
+++ src/extension.ts
@@ -44,7 +44,7 @@ export function activate(context: vscode.ExtensionContext) {
 
     const fileStrArr = fileStr.split('\n').filter((line) => line.trim());
     let arrResult = readArrayLines(fileStrArr);
-    let typesMap = new Map<string, number[]>();
+    let typesMap = new Map<string, Array<number>>();
     for (let i = 0; i < arrResult.length; i++) {
       if (reservedMainKeywords.includes(arrResult[i].line[0].value)) {
         arrResult[i].mainType = arrResult[i].line[0].value;
@@ -55,7 +55,8 @@ export function activate(context: vscode.ExtensionContext) {
       let currValue: Array<any> = [];
 
       if(typesMap.has(arrResult[i].mainType)) {
-        currValue.push(typesMap.get(arrResult[i].mainType));
+        console.log("Map content to be retrieved: ", typesMap.get(arrResult[i].mainType));
+        currValue = currValue.concat(typesMap.get(arrResult[i].mainType));
       }
 
       currValue.push(i);
@@ -67,51 +68,61 @@ export function activate(context: vscode.ExtensionContext) {
     type OrderingArray = {
       element: any,
       position: number
-    }
+    };
 
     let dataList: any[] = [];
     let varList: any[] = [];
     let resList: any[] = [];
     
+    console.log(">>>>>>>>>>>> Datas")
+    console.log(typesMap.get("data")!.length);
+    console.log(typesMap.get("data"));
     if(typesMap.has("data")) {
-      for (let i = 0; i < typesMap.get("data")!.length; i++) {
+      typesMap.get("data")!.forEach(i => {
         const item: OrderingArray = {
           element: arrResult[i],
           position: i
         };
         dataList.push(item);
-      }
+      });
 
       dataList.sort((d1, d2) => {
-        return d1.element.line[2].toLowerCase().localeCompare(d2.element.line[2].toLowerCase());
+        return d1.element.line[2].value.toLowerCase().localeCompare(d2.element.line[2].value.toLowerCase());
       });
     }
+    console.log("datalist: ", dataList);
 
+    console.log(">>>>>>>>>>>> Variables");
+    console.log(typesMap.get("variable")!.length);
+    console.log(typesMap.get("variable"));
     if(typesMap.has("variable")) {
-      for (let i = 0; i < typesMap.get("variable")!.length; i++) {
+      typesMap.get("variable")!.forEach (i => {
+        console.log("variables i= ", i);
+        console.log("arrResult: ", arrResult[i]);
         const item: OrderingArray = {
           element: arrResult[i],
           position: i
         };
         varList.push(item);
-      }
+        console.log(item);
+      });
 
       varList.sort((d1, d2) => {
-        return d1.element.line[1].toLowerCase().localeCompare(d2.element.line[1].toLowerCase());
+        return d1.element.line[1].value.toLowerCase().localeCompare(d2.element.line[1].value.toLowerCase());
       });
     }   
 
     if(typesMap.has("resource")) {
-      for (let i = 0; i < typesMap.get("resource")!.length; i++) {
+      typesMap.get("resource")!.forEach(i => {
         const item: OrderingArray = {
           element: arrResult[i],
           position: i
         };
         resList.push(item);
-      }
+      });
 
       resList.sort((d1, d2) => {
-        return d1.element.line[2].toLowerCase().localeCompare(d2.element.line[2].toLowerCase());
+        return d1.element.line[2].value.toLowerCase().localeCompare(d2.element.line[2].value.toLowerCase());
       });
     }
     
